935129b09bfa3983c433d4fb905bd2b507605657
Josiah Seaman <josiah.seaman@newlinetechnicalinnovations.com>
2013-07-30 14:28:41 -0600

    Updated character sheets for tests.  New test for weapon choosing.

--- tests.py
+++ tests.py
@@ -8,12 +8,12 @@ class SceneTest(unittest.TestCase):
 
     def testAccuracy(self):
         import ExaltedCharacter as e
-        c = e.ExaltedCharacter('Caedris.ecg')
-        self.assertEqual(c.accuracy(), 10)
+        c = e.ExaltedCharacter('CaedrisEmissaryofTenThousandWinds.ecg')
+        self.assertEqual(c.accuracy(), 9)
 
     def testDamage(self):
         import ExaltedCharacter as e
-        c = e.ExaltedCharacter('Caedris.ecg')
+        c = e.ExaltedCharacter('CaedrisEmissaryofTenThousandWinds.ecg')
         self.assertEqual(c.damageCode(), 7)
         print c.sumDicePool('Essence')
 
@@ -21,7 +21,7 @@ class SceneTest(unittest.TestCase):
         scene = Scene.CombatScene(False)
         self.assertEqual(len(scene.characters), 0)
         import ExaltedCharacter
-        c = ExaltedCharacter.ExaltedCharacter('Gin.ecg')
+        c = ExaltedCharacter.ExaltedCharacter('GintheFearlessRadianceofAwesomeHonor.ecg')
         scene.addCharacter(c)
         scene.beginBattle()
         self.assertEqual(len(scene.battleWheel.activeCharacters), 1)
@@ -50,13 +50,18 @@ class CharacterTest(unittest.TestCase):
     def testDerivedStats(self):
         print "Stat test"
         self.assertEqual(self.c['Essence'], 4)
-        self.assertEqual(self.c.peripheralEssence, 42)
-        self.assertEqual(self.c.personalEssence, 17)
+        self.assertEqual(self.c.peripheralEssence, 43)
+        self.assertEqual(self.c.personalEssence, 18)
 
     def testRunningOut(self):
         self.c.roll('Compassion', 'Dexterity')
         self.c.roll('Compassion', 'Dexterity')
         self.assertRaises(ValueError, self.c.roll, 'Compassion', 'Dexterity' )
+
+    def chooseWeaponTest(self):
+        import ExaltedCharacter
+        gin = ExaltedCharacter.ExaltedCharacter('GintheFearlessRadianceofAwesomeHonor.ecg')
+
 
 if __name__ == '__main__':
     unittest.main()
